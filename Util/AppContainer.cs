2c3e0fc934101b34411ce7298462fac60eeff47f
Sergey Semenov <Laiser399@mail.ru>
2022-10-13 02:11:54 +0300

    Add DI registration in util

--- Util/AppContainer.cs
+++ Util/AppContainer.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using Util.Services;
+using Util.Services.Abstract;
 
 namespace Util;
 
@@ -7,6 +9,15 @@ public static class AppContainer
     public static IContainer Build()
     {
         var builder = new ContainerBuilder();
+
+        builder
+            .RegisterType<CommandExecutor>()
+            .As<ICommandExecutor>()
+            .SingleInstance();
+        builder
+            .RegisterType<RemoteScannerService>()
+            .As<IRemoteScannerService>()
+            .SingleInstance();
 
         return builder.Build();
     }
