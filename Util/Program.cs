cb298919e571e7a1c95da0b38bd3753b2a102804
Sergey Semenov <Laiser399@mail.ru>
2022-10-13 02:04:01 +0300

    Add util main method realisation

--- Util/Program.cs
+++ Util/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello");
+﻿using Autofac;
+using Util;
+using Util.Services.Abstract;
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Invalid count of arguments. You must specify command and argument.");
+    return;
+}
+
+var container = AppContainer.Build();
+
+var commandExecutor = container.Resolve<ICommandExecutor>();
+
+await commandExecutor.ExecuteAsync(args[0], args[1]);
