bb3560cf7092164c9790c1dda41b55170aaafd01
Sergey Semenov <Laiser399@mail.ru>
2022-10-12 14:46:34 +0300

    Add service DI module; Register it with autofac.

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,4 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Autofac;
+using Autofac.Builder;
+using Autofac.Extensions.DependencyInjection;
+using Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(
+    ContainerBuildOptions.None,
+    x => x.RegisterModule<ServiceModule>())
+);
 
 builder.Services.AddControllers();
 
@@ -6,4 +16,4 @@ var app = builder.Build();
 
 app.MapControllers();
 
-app.Run("http://localhost:39999");
+app.Run("http://localhost:5000");
---
+++ Service/ServiceModule.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Service.Services;
+using Service.Services.Abstract;
+
+namespace Service;
+
+public class ServiceModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<ScanTasksManager>()
+            .As<IScanTasksManager>()
+            .SingleInstance();
+        builder
+            .RegisterType<ScanService>()
+            .As<IScanService>()
+            .SingleInstance();
+        builder
+            .RegisterType<FileScanner>()
+            .As<IFileScanner>()
+            .SingleInstance();
+        builder
+            .RegisterType<SuspiciousContentCheckersProvider>()
+            .As<ISuspiciousContentCheckersProvider>()
+            .SingleInstance();
+    }
+}
